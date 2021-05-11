d54fc8d35d111baf18f37b9c3d1246f80d050b7a
Mel Grosser <grosser.mel@live.de>
2021-05-11 15:13:48 +0200

    Changes

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,9 @@ import './Card.css'
 export default function Card({ name, images, types }) {
   return (
     <section className="Card">
-      <h2>{name.replace(/^./, name[0].toUpperCase())}</h2>
+      <h2 className="Card__heading">
+        {name.replace(/^./, name[0].toUpperCase())}
+      </h2>
       <ul className="Card__typeList">
         {types.map(type => {
           return <li key={type}>{type.replace(/^./, type[0].toUpperCase())}</li>
